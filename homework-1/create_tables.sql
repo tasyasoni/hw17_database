d9fdffa4e31eaa31cefd5180ae8a3eb2603d6a95
tasyasoni <tasyasoni1@gmail.com>
2023-09-23 18:49:13 +0300

    final_data

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
--- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+ 	first_name varchar(100),
+ 	last_name varchar(100),
+ 	title varchar(100),
+ 	birth_date date,
+	note text
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(7) PRIMARY KEY,
+ 	company_name text,
+	contact_name text
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+ 	customer_id text REFERENCES customers(customer_id),
+ 	employee_id int REFERENCES employees(employee_id),
+ 	order_date date,
+	ship_city text
+);
+
+
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,45 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import codecs
+import csv
+import json
+
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="Antoniya2308"
+)
+try:
+    with conn:
+        with conn.cursor() as cur:
+            with open('north_data\employees_data.csv', 'r') as file_csv:
+                next(file_csv)  # пропускаем заголовок
+                for line in file_csv:
+                    line = line.replace('"', '')
+                    file1 = open('north_data\employees.txt', 'a')
+                    file1.write(line)
+                file1.close()
+            with open('north_data\employees.txt', 'r') as file_final:
+                cur.copy_from(file_final, 'employees', sep=',')
+            with open('north_data\customers_data.csv', 'r') as file_csv:
+                next(file_csv)  # пропускаем заголовок
+                for line in file_csv:
+                    line = line.replace('"', '')
+                    file1 = open('north_data\customers.txt', 'a')
+                    file1.write(line)
+                file1.close()
+            with open('north_data\customers.txt', 'r') as file_final:
+                cur.copy_from(file_final, 'customers', sep=',')
+            with open('north_data\orders_data.csv', 'r') as file_csv:
+                next(file_csv)  # пропускаем заголовок
+                for line in file_csv:
+                    line = line.replace('"', '')
+                    file1 = open('north_data\orders.txt', 'a')
+                    file1.write(line)
+                file1.close()
+            with open('north_data\orders.txt', 'r') as file_final:
+                cur.copy_from(file_final, 'orders', sep=',')
+finally:
+    conn.close()
